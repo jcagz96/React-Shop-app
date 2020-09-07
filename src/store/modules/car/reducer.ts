1d8ed520beae04ebe55b534763ced779efc93937
jcagz96 <jcagz@iscte-iul.pt>
2020-09-07 22:25:43 +0100

    added suport to delete an item of quantity

--- src/store/modules/car/reducer.ts
+++ src/store/modules/car/reducer.ts
@@ -20,7 +20,7 @@ export default function auth(
 
       if (projectIndex > -1) {
         const newArray = state.carElements;
-        newArray[projectIndex].quantity++;
+        newArray[projectIndex].quantity += action.payload.carItem.quantity;
 
         return {
           ...state,
@@ -46,21 +46,34 @@ export default function auth(
         emptyCarFlag = true;
       }
 
-      console.log("Valor do item a remover: " + action.payload.carItem.price);
-      console.log(
-        `${state.totalPrice} - ${action.payload.carItem.price} = ${state
-          .totalPrice - action.payload.carItem.price}`,
+      const projectIndex1 = state.carElements.findIndex((carElement) =>
+        carElement.id === action.payload.carItem.id &&
+        carElement.size === action.payload.carItem.size
       );
 
-      return {
-        ...state,
-        emptyCar: emptyCarFlag,
-        carSize: state.carSize - 1,
-        totalPrice: state.totalPrice - action.payload.carItem.price,
-        carElements: state.carElements.filter((item) =>
-          item.id !== action.payload.carItem.id
-        ),
-      };
+      if (projectIndex1 > -1 && state.carElements[projectIndex1].quantity > 1) {
+        const newArray = state.carElements;
+        newArray[projectIndex1].quantity--;
+
+        return {
+          ...state,
+          emptyCar: emptyCarFlag,
+          totalPrice: state.totalPrice - action.payload.carItem.price,
+          carSize: state.carSize - 1,
+          carElements: newArray,
+        };
+      } else {
+        return {
+          ...state,
+          emptyCar: emptyCarFlag,
+          totalPrice: state.totalPrice - action.payload.carItem.price,
+          carSize: state.carSize - 1,
+          carElements: state.carElements.filter((item) =>
+            item.id !== action.payload.carItem.id
+          ),
+        };
+      }
+
     default:
       return state;
   }
